b95ca2e2a00a79979323246950168890eae2e77d
Piotr <pitarita15@gmail.com>
2026-08-01 17:13:49 +0200

    refactor(Piece): store shape as deep-copied cells, freeze type

--- src/main/java/tetris/Piece.java
+++ src/main/java/tetris/Piece.java
@@ -1,13 +1,22 @@
 package tetris;
 
 public class Piece {
-    //final int type;
+    final int type;
     int x ,y;
     int rotateState;
-    Piece(int x,int y, int rotateState){
-        //this.type=type;
+    int cells [][];
+    Piece(int x,int y, int rotateState ,int type){
         this.x=x;
         this.y=y;
-       this.rotateState=rotateState;
+        this.rotateState=rotateState;
+        cells = copyShape(Puzzle.shape[type]);
+        this.type=type;
+    }
+    private static int[][] copyShape(int[][] base) {
+        int[][] copy = new int[base.length][];
+        for (int i = 0; i < base.length; i++) {
+            copy[i] = base[i].clone();
+        }
+        return copy;
     }
 }
